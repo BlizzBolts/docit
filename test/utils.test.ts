d5ab912176787d98d87bad5d91ae71f6141448d0
phshy0607 <phshy0607@126.com>
2022-03-25 17:56:11 +0800

    chore: add test for url parsing

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,9 +1,41 @@
 import { assert, describe, it } from "vitest";
 import { removeExtention } from "../src/node/utils/index.js";
+import resolve from "resolve";
+import path from "path";
+const { sync } = resolve;
 
 describe("skipped suite", () => {
   it("removeExtention should remove extensions", () => {
     // Suite skipped, no error
     assert.equal(removeExtention("/aaa.mdx"), "/aaa");
+  });
+
+  it("resolve correct path", () => {
+    const suite = "../components/ApiTest.tsx";
+    const baseDir =
+      "/Users/hao/local/projj/github.com/phshy0607/docit/docs/features/API.mdx";
+
+    assert(
+      sync(suite, {
+        basedir: path.dirname(baseDir),
+      }),
+      "/Users/hao/local/projj/github.com/phshy0607/docit/docs/components/ApiTest.tsx"
+    );
+  });
+
+  it("resolve correct path for node_modules", () => {
+    const suite = "antd-mobile/es/index.d.ts";
+    const baseDir =
+      "/Users/hao/local/projj/github.com/phshy0607/docit/docs/features/API.mdx";
+
+    const result = sync(suite, {
+      basedir: path.dirname(baseDir),
+    });
+
+    console.log(result);
+    assert(
+      result,
+      "/Users/hao/local/projj/github.com/phshy0607/docit/docs/components/ApiTest.tsx"
+    );
   });
 });
