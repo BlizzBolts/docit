9338cdb58dc4f8923fa34ba7822e9b058f0cd8c4
phshy0607 <phshy0607@126.com>
2022-03-29 16:10:36 +0800

    feat: mobile view fine tune

--- src/client/built-in/ShowCode/styled.ts
+++ src/client/built-in/ShowCode/styled.ts
@@ -1,9 +1,5 @@
-import styled from 'styled-components';
-import { MobileView } from './types';
-
-export const RenderWindow = styled.div<MobileView>`
-  padding: 1em;
-`;
+import styled from "styled-components";
+import { MobileView } from "./types";
 
 export const ButtonContainer = styled.div<MobileView>`
   clear: both;
@@ -29,44 +25,55 @@ export const ButtonContainer = styled.div<MobileView>`
 `;
 
 export const CodeWindow = styled.div<{ show: boolean }>`
-  display: ${(props) => (props.show ? 'block' : 'none')};
+  display: ${(props) => (props.show ? "block" : "none")};
+`;
+
+export const RenderWindow = styled.div<MobileView>`
+  padding: 1em;
 `;
 
 export const ShowCodeContainer = styled.div<MobileView>`
   margin: 1rem 0;
-  border: 1px solid var(--c-border);
   border-radius: 4px;
+  border-color: var(--c-border);
+  border-width: ${(props) => (props.mobileView ? "0px" : "1px")};
+  border-style: solid;
+
+  display: ${(props) => (props.mobileView ? "flex" : "block")};
+  flex-flow: ${(props) => props.mobileView && "row-reverse"};
 
   ${CodeWindow} {
-    display: ${(props) => props.mobileView && 'block'};
-    flex: ${(props) => props.mobileView && '1 1 auto'};
-    overflow: ${(props) => props.mobileView && 'auto'};
+    display: ${(props) => props.mobileView && "block"};
+    flex: ${(props) => props.mobileView && "1 1 auto"};
+    overflow: ${(props) => props.mobileView && "auto"};
+    margin-right: ${(props) => props.mobileView && "2em"};
 
     pre {
-      height: ${(props) => props.mobileView && '100%'};
-      margin: ${(props) => props.mobileView && '0'};
-      border-top-right-radius: ${(props) => props.mobileView && '0'};
-      border-bottom-right-radius: ${(props) => props.mobileView && '0'};
+      height: ${(props) => props.mobileView && "100%"};
+      margin: ${(props) => (props.mobileView ? "0" : "1em 0 0 0")};
+      border-radius: 4px;
 
       code {
-        height: ${(props) => props.mobileView && '100%'};
+        height: ${(props) => props.mobileView && "100%"};
       }
     }
   }
 
   ${RenderWindow} {
-    flex: ${(props) => props.mobileView && '0 0 360px'};
-    width: ${(props) => props.mobileView && '360px'};
-    min-width: ${(props) => props.mobileView && '360px'};
-    man-width: ${(props) => props.mobileView && '360px'};
+    flex: ${(props) => props.mobileView && "0 0 360px"};
+    width: ${(props) => props.mobileView && "360px"};
+    min-width: ${(props) => props.mobileView && "360px"};
+    max-width: ${(props) => props.mobileView && "360px"};
 
-    height: ${(props) => props.mobileView && '640px'};
-    min-height: ${(props) => props.mobileView && '640px'};
-    max-height: ${(props) => props.mobileView && '640px'};
+    height: ${(props) => props.mobileView && "640px"};
+    min-height: ${(props) => props.mobileView && "640px"};
+    max-height: ${(props) => props.mobileView && "640px"};
 
-    border-left: ${(props) => props.mobileView && '1px solid var(--c-border)'};
-    padding: ${(props) => props.mobileView && '0'};
+    border-left: ${(props) => props.mobileView && "1px solid var(--c-border)"};
+    padding: ${(props) => props.mobileView && "0"};
 
+    border: ${(props) =>
+      props.mobileView ? "1px solid var(--c-border);" : "0px"};
 
     @media (max-width: 768px) {
       width: 100%;
@@ -75,13 +82,10 @@ export const ShowCodeContainer = styled.div<MobileView>`
   }
 
   ${ButtonContainer} {
-    display: ${(props) => props.mobileView && 'none'};
+    display: ${(props) => props.mobileView && "none"};
   }
 
-  display: ${(props) => (props.mobileView ? 'flex' : 'block')};
-  flex-flow: ${(props) => props.mobileView && 'row-reverse'};
-
   @media (max-width: 1024px) {
-    flex-flow: ${(props) => props.mobileView && 'column'};
+    flex-flow: ${(props) => props.mobileView && "column"};
   }
 `;
