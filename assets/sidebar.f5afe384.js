e8d89d1d5f33b2b456b69d109912e8e8045c3dcb
phshy0607 <phshy0607@126.com>
2022-04-19 19:48:50 +0800

    Updates

--- assets/sidebar.f5afe384.js
+++ assets/sidebar.f5afe384.js
@@ -1,4 +1,4 @@
-import{u as r,R as e}from"./index.c50dce5b.js";function m(t={}){const{wrapper:s}=Object.assign({},r(),t.components);return s?e.createElement(s,t,e.createElement(a)):a();function a(){const n=Object.assign({h1:"h1",p:"p",pre:"pre",code:"code",span:"span",h2:"h2",a:"a"},r(),t.components);return e.createElement(e.Fragment,null,e.createElement(n.h1,{id:"sidebars"},"Sidebars"),`
+import{u as r,R as e}from"./index.1d9e0a3a.js";function m(t={}){const{wrapper:s}=Object.assign({},r(),t.components);return s?e.createElement(s,t,e.createElement(a)):a();function a(){const n=Object.assign({h1:"h1",p:"p",pre:"pre",code:"code",span:"span",h2:"h2",a:"a"},r(),t.components);return e.createElement(e.Fragment,null,e.createElement(n.h1,{id:"sidebars"},"Sidebars"),`
 `,e.createElement(n.p,null,"Docit will use system file structure as sidebars."),`
 `,e.createElement(n.pre,null,e.createElement(n.code,{className:"hljs language-arduino"},`\u251C\u2500\u2500 FAQ.mdx
 \u251C\u2500\u2500 feature-request.mdx
