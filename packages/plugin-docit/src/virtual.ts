9b030269231fa2b5bbbebb7081c28797fc5b441c
citrus327 <citrus327@outlook.com>
2023-09-18 17:06:53 +0800

    feat: remove uselss options

--- packages/plugin-docit/src/virtual.ts
+++ packages/plugin-docit/src/virtual.ts
@@ -1,10 +1,7 @@
 import type { PluginOption } from "vite";
-import { logger } from "@blizzbolts/docit-shared";
-import type { DocitPluginOption } from "./types";
 
-export const virtual = (option: DocitPluginOption): PluginOption => {
+export const virtual = (): PluginOption => {
   const cache = new Map<string, string>();
-  logger.withTag("virtual").info(option);
   return {
     name: "vite-plugin-docit-virtual",
     resolveId(id) {
