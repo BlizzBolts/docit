fb4c4b03a9f0f94465a4f4235a300e35acf94562
citrus327 <citrus327@outlook.com>
2023-09-21 14:24:44 +0800

    chore: tmp-dir

--- test/context/tmp-dir.ts
+++ test/context/tmp-dir.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import type { DirectoryResult } from "tmp-promise";
 import { beforeEach, afterEach } from "vitest";
 import { dir } from "tmp-promise";
@@ -5,16 +6,19 @@ import fsx from "fs-extra";
 
 export interface TmpDirContext {
   tmp: DirectoryResult;
+  r: (p: string) => string;
 }
 
 export const setupTmpDir = () => {
   beforeEach<TmpDirContext>(async (context) => {
     const result = await dir();
     context.tmp = result;
+    context.r = (p: string) => path.resolve(result.path, p);
   });
 
   afterEach<TmpDirContext>(async (context) => {
     await fsx.emptyDir(context.tmp.path);
     context.tmp.cleanup();
+    context.r = () => "";
   });
 };
