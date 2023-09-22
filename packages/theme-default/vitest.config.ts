8903b9de1d1d3d5c6b5ccc8e3d11f757a89fe456
citrus327 <citrus327@outlook.com>
2023-09-22 15:38:50 +0800

    chore: testui

--- packages/theme-default/vitest.config.ts
+++ packages/theme-default/vitest.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   test: {
     environment: "jsdom",
+    exclude: ["styled-system/**", "node_modules/**"],
   },
   plugins: [tsconfigPaths()],
 });
