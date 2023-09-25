3fc98c231dad577d8a45efb90a4ff406010bef8e
citrus327 <citrus327@outlook.com>
2023-09-25 14:28:40 +0800

    feat: template

---
+++ packages/core/node/init/template.ts
@@ -0,0 +1,32 @@
+import path from "node:path";
+import type { DocitConfig } from "@blizzbolts/docit-shared";
+import { zScaffoldOptions, type ScaffoldOptions, zDocitConfig } from "@blizzbolts/docit-shared";
+import { getPreflightConfig } from "../preflight";
+
+export interface TemplateFile {
+  content: string;
+  location: string;
+}
+
+export const makeDocitConfigFile = async (
+  scaffoldOptions: ScaffoldOptions,
+): Promise<TemplateFile> => {
+  const options = zScaffoldOptions.parse(scaffoldOptions);
+
+  const config = await getPreflightConfig(scaffoldOptions.root);
+  const fileExtension = config.isEsm ? ".js" : config["tsconfig.json"] ? ".ts" : ".mjs";
+  const filename = `docit.config${fileExtension}`;
+  const fileLocation = path.resolve(scaffoldOptions.root, "./", filename);
+
+  const docitConfig: DocitConfig = zDocitConfig.parse({
+    site: {
+      title: options.title,
+      description: options.description,
+    },
+  });
+
+  return {
+    content: `export default ${JSON.stringify(docitConfig, null, 2)}`,
+    location: fileLocation,
+  };
+};
---
+++ packages/core/test/template.test.ts
@@ -0,0 +1,47 @@
+import { setupTmpDir, type TmpDirContext } from "@workspace/test/context/tmp-dir";
+import { describe, it } from "vitest";
+import fsx from "fs-extra";
+import { readConfigFromFile } from "../node/config";
+import { makeDocitConfigFile } from "../node/init/template";
+
+describe.concurrent("make correct template file", () => {
+  setupTmpDir();
+  it<TmpDirContext>("should make correct docit.config file without any config", async ({
+    expect,
+    r,
+    maker,
+  }) => {
+    await maker.makePackageJson({ type: "module" });
+    const result = await makeDocitConfigFile({
+      root: r(),
+    });
+
+    await fsx.outputFile(result.location, result.content);
+    const readConfig = await readConfigFromFile(r());
+    expect({
+      base: "/",
+      docRoot: "./docs",
+      outDir: "./docit/build",
+      root: "./",
+      site: {},
+    }).toEqual(readConfig);
+    expect(result.location).toEqual(r(result.location));
+  });
+
+  it<TmpDirContext>("should make correct docit.config file", async ({ expect, r, maker }) => {
+    await maker.makePackageJson({ type: "module" });
+    const result = await makeDocitConfigFile({
+      title: "My Site",
+      description: "My Site Description",
+      root: r(),
+    });
+
+    await fsx.outputFile(result.location, result.content);
+    const readConfig = await readConfigFromFile(r());
+    expect({
+      title: "My Site",
+      description: "My Site Description",
+    }).toEqual(readConfig.site);
+    expect(result.location).toEqual(r(result.location));
+  });
+});
