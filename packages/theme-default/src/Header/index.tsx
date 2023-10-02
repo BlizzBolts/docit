5e6aaed1af11ef500abe7c7c789ecac9460b53f9
citrus327 <citrus327@outlook.com>
2023-10-02 19:14:47 +0800

    feat: version select

--- packages/theme-default/src/Header/index.tsx
+++ packages/theme-default/src/Header/index.tsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
 import type { HeaderProps } from "./types";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/select";
 import { Button } from "@/components/button";
 
 const Header: React.FC<HeaderProps> = (props) => {
   const { title, navs, onNavigate } = props;
+  const [version, setVersion] = useState("1.0.0");
   return (
-    <div className="px-4 md:px-8 lg:px-12 xl:px-16 flex items-center h-12 justify-between shadow-sm">
-      <h3 className="scroll-m-20 text-xl font-semibold tracking-tight font-mono">{title} </h3>
+    <div className="px-8 md:px-12 flex items-center h-12 justify-between shadow-sm">
+      <h3 className="scroll-m-20 text-xl font-semibold tracking-tight font-mono text-primary/100">
+        {title}
+      </h3>
+      <Select value={version} onValueChange={setVersion}>
+        <SelectTrigger className="w-auto outline-none h-6 mr-auto ml-2 bg-primary/5">
+          <SelectValue placeholder="Select Version" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="1.0.0">v1.0.0</SelectItem>
+          <SelectItem value="1.1.0">v1.1.0</SelectItem>
+          <SelectItem value="2.0.0">v2.0.0</SelectItem>
+        </SelectContent>
+      </Select>
+
       <div className="flex items-center">
         {navs?.map((o) => {
           return (
-            <Button variant="link" key={o.url} onClick={() => onNavigate(o)}>
+            <Button variant="link" key={o.url} onClick={() => onNavigate(o)} className="text-black">
               {o.title}
             </Button>
           );
