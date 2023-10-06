1c4659379f2f42badb085b08a94ca3830450573f
citrus327 <citrus327@outlook.com>
2023-10-06 19:02:38 +0800

    feat: support ts type config file

--- packages/shared/test/config.test.ts
+++ packages/shared/test/config.test.ts
@@ -5,6 +5,7 @@ import { setupTmpDir } from "@workspace/test/context/tmp-dir";
 import { coreLogger } from "../src/shared/logger";
 import { findConfigFile, readConfigFromFile, resolveConfig } from "../src/node/config";
 import { init } from "../../core/node/init";
+import type { DocitConfig } from "@/shared";
 
 describe.concurrent("findConfigFile", () => {
   describe.concurrent("ESM env", () => {
@@ -17,16 +18,22 @@ describe.concurrent("findConfigFile", () => {
       },
     });
 
-    it<TmpDirContext>("find js", async ({ tmp, r, expect }) => {
-      await fsx.outputFile(r("./docit.config.js"), `export default {from: "js"}`);
+    it<TmpDirContext>("find js", async ({ tmp, r, expect, maker }) => {
+      await maker.makeDocitConfigFile(r("./docit.config.js"), {}, "esm");
       const config = await findConfigFile(tmp.path);
       expect(config).toBe("docit.config.js");
     });
 
-    it<TmpDirContext>("find cjs", async ({ tmp, r, expect }) => {
-      await fsx.outputFile(r("./docit.config.cjs"), `export default {from: "js"}`);
+    it<TmpDirContext>("find cjs", async ({ tmp, r, expect, maker }) => {
+      await maker.makeDocitConfigFile(r("./docit.config.cjs"), {}, "cjs");
       const config = await findConfigFile(tmp.path);
       expect(config).toBe("docit.config.cjs");
+    });
+
+    it<TmpDirContext>("find ts", async ({ tmp, r, expect, maker }) => {
+      await maker.makeDocitConfigFile(r("./docit.config.ts"), {}, "esm");
+      const config = await findConfigFile(tmp.path);
+      expect(config).toBe("docit.config.ts");
     });
   });
   describe.concurrent("CJS env", () => {
@@ -39,16 +46,22 @@ describe.concurrent("findConfigFile", () => {
       },
     });
 
-    it<TmpDirContext>("find js", async ({ tmp, r, expect }) => {
-      await fsx.outputFile(r("./docit.config.js"), `module.exports = {from: "js"}`);
+    it<TmpDirContext>("find js", async ({ tmp, r, expect, maker }) => {
+      await maker.makeDocitConfigFile(r("./docit.config.js"), {}, "cjs");
       const config = await findConfigFile(tmp.path);
       expect(config).toBe("docit.config.js");
     });
 
-    it<TmpDirContext>("find mjs", async ({ tmp, r, expect }) => {
-      await fsx.outputFile(r("./docit.config.mjs"), `export default {from: "js"}`);
+    it<TmpDirContext>("find mjs", async ({ tmp, r, expect, maker }) => {
+      await maker.makeDocitConfigFile(r("./docit.config.mjs"), {}, "esm");
       const config = await findConfigFile(tmp.path);
       expect(config).toBe("docit.config.mjs");
+    });
+
+    it<TmpDirContext>("find ts", async ({ tmp, r, expect, maker }) => {
+      await maker.makeDocitConfigFile(r("./docit.config.ts"), {}, "esm");
+      const config = await findConfigFile(tmp.path);
+      expect(config).toBe("docit.config.ts");
     });
   });
 });
@@ -59,7 +72,7 @@ describe.concurrent("readConfigFromFile", async () => {
     it<TmpDirContext>("should return null when no config file", async ({ tmp, expect }) => {
       const spy = vi.spyOn(coreLogger, "warn");
       const config = await readConfigFromFile(tmp.path);
-      expect(config).toEqual(null);
+      expect(config).toStrictEqual(null);
       expect(spy).toBeCalledTimes(0);
     });
     it<TmpDirContext>("should return null and log when failed to parse config file", async ({
@@ -70,7 +83,7 @@ describe.concurrent("readConfigFromFile", async () => {
       await fsx.outputFile(r("./docit.config.mjs"), `export default {from: `);
       const spy = vi.spyOn(coreLogger, "warn");
       const config = await readConfigFromFile(tmp.path);
-      expect(config).toEqual(null);
+      expect(config).toStrictEqual(null);
       expect(spy).toBeCalledTimes(1);
     });
   });
@@ -85,104 +98,45 @@ describe.concurrent("readConfigFromFile", async () => {
           await fsx.remove(r("./package.json"));
         },
       });
-      it<TmpDirContext>("parse js", async ({ tmp, r, expect }) => {
-        await fsx.outputFile(
-          r("./docit.config.js"),
-          `export default {
-            "base": "/",
-            "docRoot": "./docs",
-            "outDir": "./docit/build",
-            "root": "./",
-            "site": {
-              "description": "Site Description",
-              "theme": "default",
-              "title": "Vitest",
-            },
-          }`,
-        );
+      it<TmpDirContext>("parse js", async ({ tmp, r, expect, maker }) => {
+        await maker.makeDocitConfigFile(r("./docit.config.js"), { base: "./js" }, "esm");
         const config = await readConfigFromFile(tmp.path);
-        expect(config).toEqual({
-          base: "/",
-          docRoot: "./docs",
-          outDir: "./docit/build",
-          root: "./",
-          site: {
-            description: "Site Description",
-            theme: "default",
-            title: "Vitest",
-          },
-        });
+        expect(config).toStrictEqual({ base: "./js" });
       });
-      it<TmpDirContext>("parse cjs", async ({ tmp, r, expect }) => {
-        await fsx.outputFile(
-          r("./docit.config.cjs"),
-          `module.exports = {
-            "base": "/",
-            "docRoot": "./docs",
-            "outDir": "./docit/build",
-            "root": "./",
-            "site": {
-              "description": "Site Description",
-              "theme": "default",
-              "title": "Vitest",
-            },
-          }`,
-        );
+      it<TmpDirContext>("parse cjs", async ({ tmp, r, expect, maker }) => {
+        await maker.makeDocitConfigFile(r("./docit.config.cjs"), { base: "./cjs" }, "cjs");
         const config = await readConfigFromFile(tmp.path);
-        expect(config).toEqual({
-          base: "/",
-          docRoot: "./docs",
-          outDir: "./docit/build",
-          root: "./",
-          site: {
-            description: "Site Description",
-            theme: "default",
-            title: "Vitest",
-          },
-        });
+        expect(config).toStrictEqual({ base: "./cjs" });
       });
-
-      it.todo<TmpDirContext>("parse ts", async ({ tmp, r, expect }) => {
-        // FIXME:
-        // await fsx.outputJson(r("./tsconfig.json"), {
-        //   compilerOptions: {
-        //     module: "ESNext",
-        //     target: "ESNext",
-        //     moduleResolution: "Node",
-        //     resolveJsonModule: true,
-        //     esModuleInterop: true,
-        //     // stricts
-        //     strict: true,
-        //     strictNullChecks: true,
-        //     skipLibCheck: true,
-        //   },
-        // });
-        await fsx.outputFile(
-          r("./docit.config.ts"),
-          `export default {
-            "base": "/",
-            "docRoot": "./docs",
-            "outDir": "./docit/build",
-            "root": "./",
-            "site": {
-              "description": "Site Description",
-              "theme": "default",
-              "title": "Vitest",
-            },
-          }`,
-        );
+      it<TmpDirContext>("parse ts", async ({ tmp, r, expect, maker }) => {
+        await maker.makeDocitConfigFile(r("./docit.config.ts"), { base: "./ts" }, "esm");
         const config = await readConfigFromFile(tmp.path);
-        expect(config).toEqual({
-          base: "/",
-          docRoot: "./docs",
-          outDir: "./docit/build",
-          root: "./",
-          site: {
-            description: "Site Description",
-            theme: "default",
-            title: "Vitest",
-          },
-        });
+        expect(config).toStrictEqual({ base: "./ts" });
+      });
+    });
+    describe("CJS env", () => {
+      setupTmpDir({
+        before: async ({ r }) => {
+          await fsx.outputJson(r("./package.json"), { type: "commonjs" });
+        },
+        after: async ({ r }) => {
+          await fsx.remove(r("./package.json"));
+        },
+      });
+      it<TmpDirContext>("parse js", async ({ tmp, r, expect, maker }) => {
+        await maker.makeDocitConfigFile(r("./docit.config.js"), { base: "./js" }, "cjs");
+        const config = await readConfigFromFile(tmp.path);
+        expect(config).toStrictEqual({ base: "./js" });
+      });
+      it<TmpDirContext>("parse mjs", async ({ tmp, r, expect, maker }) => {
+        await maker.makeDocitConfigFile(r("./docit.config.cjs"), { base: "./mjs" }, "esm");
+        const config = await readConfigFromFile(tmp.path);
+        expect(config).toStrictEqual({ base: "./mjs" });
+      });
+      it<TmpDirContext>("parse ts", async ({ tmp, r, expect, maker }) => {
+        await maker.makeDocitConfigFile(r("./docit.config.ts"), { base: "./ts" }, "esm");
+        const config = await readConfigFromFile(tmp.path);
+        expect(config).toStrictEqual({ base: "./ts" });
       });
     });
   });
@@ -198,8 +152,13 @@ describe.concurrent("resolveConfig", () => {
 
     const config = await resolveConfig(r());
 
-    expect(config.docRoot).toBe(r("./docs"));
-    expect(config.root).toBe(r());
-    expect(config.outDir).toBe(r("./docs/dist"));
+    expect(config).toStrictEqual({
+      docRoot: r("./docs"),
+      root: r(),
+      outDir: r("./docs/dist"),
+      server: {
+        port: 3000,
+      },
+    } as DocitConfig);
   });
 });
